4c5a81a992587c0becdbb3ce185905a9d7cbc92f
Muhammad Awais <muhammad.awais@enabling-systems.com-at-553960022444>
2025-11-24 18:28:50 +0500

    some changes

--- MountUtility/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿window.triggerFileDownloadUrl = function (url) {
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = "";  // let the server decide filename
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-};
